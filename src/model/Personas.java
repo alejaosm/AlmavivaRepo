d69e8ecb604231d9ffd924e99a56171858780d6f
Maleja Gosman <Maleja Gosman@192.168.1.9>
2018-05-16 02:53:55 -0500

    Creación

---
+++ src/model/Personas.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Maleja Gosman
+ */
+public class Personas {
+
+    private int Id_Persona;
+    private String Nombres_Raz;
+    private String Apellidos;
+    private int Numero_Doc;
+    private String Correo;
+    private String Direccion;
+    private String Telefono;
+    private String Ubicacion;
+    private String Rep_Legal;
+    private String Estado;
+    private String Tipo_Persona;
+    private String Tipo_Doc;
+
+    public int getId_Persona() {
+        return Id_Persona;
+    }
+
+    public void setId_Persona(int Id_Persona) {
+        this.Id_Persona = Id_Persona;
+    }
+
+    public String getNombres_Raz() {
+        return Nombres_Raz;
+    }
+
+    public void setNombres_Raz(String Nombres_Raz) {
+        this.Nombres_Raz = Nombres_Raz;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public int getNumero_Doc() {
+        return Numero_Doc;
+    }
+
+    public void setNumero_Doc(int Numero_Doc) {
+        this.Numero_Doc = Numero_Doc;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getUbicacion() {
+        return Ubicacion;
+    }
+
+    public void setUbicacion(String Ubicacion) {
+        this.Ubicacion = Ubicacion;
+    }
+
+    public String getRep_Legal() {
+        return Rep_Legal;
+    }
+
+    public void setRep_Legal(String Rep_Legal) {
+        this.Rep_Legal = Rep_Legal;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+
+    public String getTipo_Persona() {
+        return Tipo_Persona;
+    }
+
+    public void setTipo_Persona(String Tipo_Persona) {
+        this.Tipo_Persona = Tipo_Persona;
+    }
+
+    public String getTipo_Doc() {
+        return Tipo_Doc;
+    }
+
+    public void setTipo_Doc(String Tipo_Doc) {
+        this.Tipo_Doc = Tipo_Doc;
+    }
+    
+    
+    
+    
+    
+}
