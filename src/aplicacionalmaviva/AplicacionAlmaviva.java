37b3bb06757e5e1935d9969e7655e6f0266ae15e
Maria Alejandra Gómez Osman <alejaosm19@gmail.com>
2018-05-16 02:12:47 -0500

    comentario

--- src/aplicacionalmaviva/AplicacionAlmaviva.java
+++ src/aplicacionalmaviva/AplicacionAlmaviva.java
@@ -16,7 +16,7 @@ public class AplicacionAlmaviva {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hola");
+        System.out.print("holaaaaaaa");
     }
     
 }
