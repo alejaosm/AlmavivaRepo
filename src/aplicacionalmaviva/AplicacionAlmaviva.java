686474f28f9707434ccbeb575f08e768bb8e5b42
Maleja Gosman <Maleja Gosman@192.168.1.9>
2018-05-16 19:19:16 -0500

    Creación

--- src/aplicacionalmaviva/AplicacionAlmaviva.java
+++ src/aplicacionalmaviva/AplicacionAlmaviva.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aplicacionalmaviva;
 
 /**
@@ -11,9 +7,7 @@ package aplicacionalmaviva;
  */
 public class AplicacionAlmaviva {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
 
